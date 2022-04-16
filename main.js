e7298d310decf4cc1498f5b7bd47ecca34cc2591
VICTOR ANTONIO SILVA FRANCO <VICTOR.FRANCO@KOTHE.LOCAL>
2022-04-16 10:25:54 -0300

    First Step in JavaScript

---
+++ main.js
@@ -0,0 +1,13 @@
+// DECLARANDO UMA CONSTANTE COM FUNÇÃO:
+/*
+const pessoa = {
+  name: 'victor',
+  age: 19,
+  falar: function() {
+    alert(pessoa.age)
+  }
+}
+
+pessoa.falar()
+*/
+// DOM: Document Object Model
