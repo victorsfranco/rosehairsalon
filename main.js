39882b108e7cbd7fdc2afca3bb731cb64ca4aafa
victorfranco <victorsfranco@outlook.com>
2022-04-18 00:43:50 -0300

    Making toggle menu by clicking

--- main.js
+++ main.js
@@ -10,4 +10,24 @@ const pessoa = {
 
 pessoa.falar()
 */
-// DOM: Document Object Model
+// DOM - Document Object Model:
+
+
+/* Toggle menu by clicking*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for(const element of toggle) {
+  element.addEventListener('click', function() {
+    nav.classList.toggle('show')
+  })
+}
+
+/* Hide menu by clicking on nav links*/
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links) {
+  link.addEventListener('click', function() {
+    nav.classList.remove('show')
+  })
+}
